de82e9f7662f4e4d1ac3e831f3a2582a0fe32aa4
Forrest Sutton <forrest_sutton@yahoo.com>
2014-11-03 19:07:04 +0000

    add templates and test folders

---
+++ README.md
@@ -0,0 +1,22 @@
+#ncrm
+====
+##aka new crm 
+
+a python google app engine crm with enhacments
+google app engine using python - using data store and adding analytics based datastore
+
+
+###to do
+add data models
+    *user admins - who access and how
+    *name and quick data (email, contact, phone)
+    *mail address
+    *note
+    *sales info - products catelog, info about environs 
+    *sales data - data warehouse 
+
+templates
+    add wtforms
+    *user
+    *name and qd
+    
--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
-app.config['DEBUG'] = True
+import webapp2
+import os
+from models import File
+#from views import MainPage, About, New
+
+# from flask import Flask
+# app = Flask(__name__)
+# app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!')
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
 
 
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+
+app = webapp2.WSGIApplication([('/', MainPage), ], debug=True)
