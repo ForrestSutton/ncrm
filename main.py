9b76ed0b08ba470bf86bbb8e3afe7bfbfbd7096f
Forrest Sutton <forrestsutton@gmail.com>
2014-11-05 08:55:46 -0500

    fix merge

--- main.py
+++ main.py
@@ -1,21 +1,12 @@
+from google.appengine.api import user
 import webapp2
-import os
-from models import File
-#from views import MainPage, About, New
 
-# from flask import Flask
-# app = Flask(__name__)
-# app.config['DEBUG'] = True
-
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('Hello, World!')
 
-
-
-
-app = webapp2.WSGIApplication([('/', MainPage), ], debug=True)
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
