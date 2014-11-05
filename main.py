f7a12e9eb70d89ca6ddc17b0a6b7bd7ebdef8a9c
Forrest Sutton <forrestsutton@gmail.com>
2014-11-05 14:16:35 -0500

    get boiler plate code working

--- main.py
+++ main.py
@@ -1,23 +1,93 @@
-from google.appengine.api import users 
+import cgi
+import urllib
 import webapp2
 
-class MainPage(webapp2.RequestHandler):
- def get(self):
-        # [START get_current_user]
-        # Checks for active Google account session
-        user = users.get_current_user()
-        # [END get_current_user]
+from google.appengine.api import users
+from google.appengine.ext import ndb
 
-        # [START if_user]
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.write('Hello, ' + user.nickname())
-        # [END if_user]
-        # [START if_not_user]
+
+
+MAIN_PAGE_FOOTER_TEMPLATE = """\
+  <form action="/sign?%s" method="post">
+    <div><textarea name="content" rows="3" cols="60"></textarea></div>
+    <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+    <hr>
+    <form>Guestbook name:
+    <input value="%s" name="guestbook_name">
+    <input type="submit" value="switch">
+    </form>
+    <a href="%s">%s</a>
+    </body>
+  </html>
+"""
+DEFAULT_GUESTBOOK_NAME = 'default_guestbook'
+
+def guestbook_key(guestbook_name=DEFAULT_GUESTBOOK_NAME):
+    """contructs a datastore key for a guestbook entity with guestbook_name"""
+    return ndb.Key('Guestbook', guestbook_name)
+
+
+class Greeting(ndb.Model):
+   """models an individual guestbook entry"""
+   author = ndb.UserProperty()
+   content = ndb.StringProperty(indexed=False)
+   date = ndb.DateTimeProperty(auto_now_add=True)
+
+
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('<html><body>')
+        guestbook_name = self.request.get('guestbook_name',
+                                            DEFAULT_GUESTBOOK_NAME)
+
+
+        greetings_query = Greeting.query(
+        ancestor=guestbook_key(guestbook_name)).order(-Greeting.date)
+        greetings = greetings_query.fetch(10)
+        # [END query]
+
+        for greeting in greetings:
+          if greeting.author:
+              self.response.write(
+                      '<b>%s</b> wrote:' % greeting.author.nickname())
+          else:
+              self.response.write('An anonymous person wrote:')
+          self.response.write('<blockquote>%s</blockquote>' %
+                              cgi.escape(greeting.content))
+
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
         else:
-            self.redirect(users.create_login_url(self.request.uri))
-        # [END if_not_user]
+            url = user.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        sign_query_params = urllib.urlencode({'guestbook_name': guestbook_name})
+        self.response.write(MAIN_PAGE_FOOTER_TEMPLATE %
+                            (sign_query_params, cgi.escape(guestbook_name),
+                            url, url_linktext))
+
+class Guestbook(webapp2.RequestHandler):
+    def post(self):
+      guestbook_name = self.request.get('guestbook_name',
+                                          DEFAULT_GUESTBOOK_NAME)
+      greeting = Greeting(parent=guestbook_key(guestbook_name))
+
+      if users.get_current_user():
+          greeting.author = users.get_current_user()
+
+      greeting.content = self.request.get('content')
+      greeting.put()
+
+
+      query_params = {'guestbook_name': guestbook_name}
+      self.redirect('/?' + urllib.urlencode(query_params))
+         # [END guestbook]
+
 
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
+('/', MainPage),
+('/sign', Guestbook),
 ], debug=True)
