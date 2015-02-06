b21a02a1281db1162239a9dbbd169c8e0839d6bd
Forrest Sutton <forrestsutton@gmail.com>
2015-02-06 15:01:20 -0500

    test not  working need to find workaround for testbeb

--- test/test.py
+++ test/test.py
@@ -7,7 +7,7 @@ TODO: These tests need to be updated to support the Python 2.7 runtime
 import os
 import unittest
 
-from google.appengine.ext import testbed
+#from google.appengine.ext import testbed
 from application import app
 
 
